97ee16af09b466be3a72057f1db8cf9739972861
Msnewbee <bilariko2@gmail.com>
2025-03-07 12:05:12 +0700

    Update VideoPlayer.jsx

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,16 +1,11 @@
-import React from "react";
-import VideoJS from "video.js"; // Pastikan sudah menginstal Video.js
-import "video.js/dist/video-js.css";
-
 export default function VideoPlayer({ videoUrl }) {
   return (
-    <div className="video-container">
-      <video
-        controls
-        className="video-js vjs-default-skin"
-        data-setup="{}"
+    <div className="relative w-full aspect-video">
+      <iframe
         src={videoUrl}
-      ></video>
+        className="w-full h-full"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
