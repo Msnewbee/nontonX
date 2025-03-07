0a919b4414b069066801e1c1f5fb60b1db896524
Msnewbee <bilariko2@gmail.com>
2025-03-07 17:07:44 +0700

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <footer className="bg-secondary text-white p-4">
+            <div className="container mx-auto text-center">
+                <p>&copy; 2025 NontonX. All rights reserved.</p>
+                <div className="flex justify-center space-x-4 mt-2">
+                    <a href="https://twitter.com/nontonx" className="hover:text-accent">Twitter</a>
+                    <a href="https://facebook.com/nontonx" className="hover:text-accent">Facebook</a>
+                    <a href="https://instagram.com/nontonx" className="hover:text-accent">Instagram</a>
+                </div>
+                <div className="mt-2">
+                    <a href="/help" className="hover:text-accent">Help</a>
+                </div>
+            </div>
+        </footer>
+    );
+}
