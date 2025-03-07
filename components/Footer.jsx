b8d9cafe2400521afa4541c5e74d261db7bbefaf
Msnewbee <bilariko2@gmail.com>
2025-03-07 17:34:14 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,15 @@
 export default function Footer() {
     return (
-        <footer className="bg-secondary text-white p-4">
+        <footer className="footer p-4">
             <div className="container mx-auto text-center">
                 <p>&copy; 2025 NontonX. All rights reserved.</p>
                 <div className="flex justify-center space-x-4 mt-2">
-                    <a href="https://twitter.com/nontonx" className="hover:text-accent">Twitter</a>
-                    <a href="https://facebook.com/nontonx" className="hover:text-accent">Facebook</a>
-                    <a href="https://instagram.com/nontonx" className="hover:text-accent">Instagram</a>
+                    <a href="https://twitter.com/nontonx" className="hover:accent">Twitter</a>
+                    <a href="https://facebook.com/nontonx" className="hover:accent">Facebook</a>
+                    <a href="https://instagram.com/nontonx" className="hover:accent">Instagram</a>
                 </div>
                 <div className="mt-2">
-                    <a href="/help" className="hover:text-accent">Help</a>
+                    <a href="/help" className="hover:accent">Help</a>
                 </div>
             </div>
         </footer>
