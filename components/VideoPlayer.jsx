95998759240e59d5fed11b83c9b3d271b3759679
Msnewbee <bilariko2@gmail.com>
2025-03-07 13:41:47 +0700

    Update VideoPlayer.jsx

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,6 +1,6 @@
 export default function VideoPlayer({ videoUrl }) {
   return (
-    <div className="relative w-full aspect-video bg-black rounded-lg overflow-hidden">
+    <div className="relative w-full aspect-video bg-black rounded-lg overflow-hidden shadow-lg">
       <video
         src={videoUrl}
         className="w-full h-full"
