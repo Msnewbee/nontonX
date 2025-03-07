9edc6d45fdcfdec9a2b11ee37e692b7ac78ad2fa
Msnewbee <bilariko2@gmail.com>
2025-03-07 17:34:56 +0700

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <nav className="bg-primary text-white p-4">
+        <nav className="navbar p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-2xl font-bold">
                     <Link href="/">NontonX</Link>
@@ -10,17 +10,17 @@ export default function Navbar() {
                 <ul className="flex space-x-4">
                     <li>
                         <Link href="/">
-                            <a className="hover:text-accent">Home</a>
+                            <a className="hover:accent">Home</a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/videos">
-                            <a className="hover:text-accent">Videos</a>
+                            <a className="hover:accent">Videos</a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/contact">
-                            <a className="hover:text-accent">Contact</a>
+                            <a className="hover:accent">Contact</a>
                         </Link>
                     </li>
                 </ul>
