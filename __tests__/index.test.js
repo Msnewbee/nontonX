1aa1702fc10c3b6d1a47c568a121fb621603446c
Msnewbee <bilariko2@gmail.com>
2025-03-08 21:35:41 +0700

    Delete __tests__ directory

--- __tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-// __tests__/index.test.js
-import { render, screen } from '@testing-library/react';
-import Home from '../pages/index';
-
-test('renders homepage', () => {
-  render(<Home />);
-  const heading = screen.getByText(/welcome to nontonx/i);
-  expect(heading).toBeInTheDocument();
-});
